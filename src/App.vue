9f316a64b0b04a09cc8421aaf88f8081eb00f785
panpan <zeyupan@163.com>
2023-07-03 11:15:45 +0800

    创建一个基本的cesium

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { onMounted } from "vue";
+import HelloCesium from "./components/HelloCesium.vue";
 import * as Cesium from "cesium";
 import "./Widgets/widgets.css";
 
-console.log(Cesium.Viewer);
+console.log("Cesium.Viewer:", Cesium.Viewer);
 window.CESIUM_BASE_URL = "/";
 // 设置相机的默认视角
 Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
@@ -22,7 +23,7 @@ Cesium.Ion.defaultAccessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlNTdjYWQxNy02MTRmLTQ4MTMtOWNjMC0zNDg2N2M4YWI1YTQiLCJpZCI6MTQ5NjE5LCJpYXQiOjE2ODc4NTY2NDJ9.wQCXDM5G0L5FNVvIDzR5nYe6J5I_zzo8JEn6w7QQ0Is";
 
 onMounted(() => {
-  let viewer = new Cesium.Viewer("cesiumContanier", {
+  window.viewer = new Cesium.Viewer("cesiumContanier", {
     infoBox: false,
     geocoder: false, // 是否显示搜索框按钮
     homeButton: false, // 是否显示home按钮
@@ -32,6 +33,15 @@ onMounted(() => {
     animation: false, // 是否显示播放动画
     timeline: false, // 是否显示时间轴
     fullscreenButton: false, // 是否显示全屏按钮
+    // 设置显示地形
+    terrainProvider: Cesium.createWorldTerrain({
+      requestVertexNormals: true, // 设置光照效果
+      requestWaterMask: true, // 设置水波纹效果
+    }),
+    sceneMode: Cesium.SceneMode.SCENE3D, // 默认值
+    // creditContainer:document.createElement('div')
+    vrButton: true,
+    scene3DOnly: true, // 节省GPU内存，让只支持3D模式
     // 设置天空盒子
     // skyBox: new Cesium.skyBox({
     //   source: {
@@ -43,20 +53,24 @@ onMounted(() => {
     //     negativeZ: "",
     //   },
     // }),
-
-    // 设置显示地形
-    terrainProvider: Cesium.createWorldTerrain({
-      requestVertexNormals: true, // 设置光照效果
-      requestWaterMask: true, // 设置水波纹效果
-    }),
   });
 
-  viewer.cesiumWidget.creditContainer.style.display = "none";
+  viewer.cesiumWidget.creditContainer.style.display = "none"; // 隐藏logo
   console.log(viewer);
+
+  // 屏幕坐标系转二维笛卡尔坐标系  Cartesian2
+  // 地理坐标系转WGS-84 Cartographic
+  // 笛卡尔转空间直角坐标系 Cartesian3
+
+  // 角度与弧度的转换
+  var radians = Cesium.Math.toRadians(90);
+  console.log(radians);
 });
+
 </script>
 
 <template>
+  <HelloCesium></HelloCesium>
   <div id="cesiumContanier"></div>
 </template>
 
--- src/components/HelloCesium.vue
+++ src/components/HelloCesium.vue
@@ -1,11 +1,18 @@
 <template>
-
+  <div v-for="(item, index) in list" class="item" :key="index">
+    <div>{{ item.name }}</div>
+  </div>
+  <button @click="add" v-teleport="false">push</button>
 </template>
 
-<script lang="ts" setup>
+<script setup>
+import { ref, nextTick } from "vue";
+const list = ref([{ name: 1 }, { name: 2 }]);
 
+function add() {
+  list.value.push({ name: 3 });
+}
+console.log(123);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
